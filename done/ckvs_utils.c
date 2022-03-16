6f9baccf1b290754c6ec4ce7e5b3f04f952a742b
jchaverot <jeremy.chaverot@epfl.ch>
2022-03-16 10:01:14 +0100

    added the ckvs_utils.c

--- done/ckvs_utils.c
+++ done/ckvs_utils.c
@@ -2,28 +2,20 @@
 // Created by trous on 15.03.2022.
 //
 
-
 void print_SHA(const char *prefix, const struct ckvs_sha *sha) {
     char buffer[SHA256_PRINTED_STRLEN];
-
     SHA256_to_string(sha, buffer);
-
     pps_printf("%-5s: %s\n", prefix, buffer);
-}
-
 }
 
 
 void SHA256_to_string(const struct ckvs_sha *sha, char *buf) {
-    hex_encode();
-
-
+    hex_encode(sha.sha, SHA256_DIGEST_LENGTH, buf);
 }
 
 void hex_encode(const uint8_t *in, size_t len, char *buf) {
     for (size_t i = 0; i < len; ++i) {
-        buf[i] = "%02x", SHA[i]);
+        sprintf(buf, "%02x", in));
     }
-
 }
 
