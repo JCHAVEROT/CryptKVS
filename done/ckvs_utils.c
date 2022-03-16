db5edb6f1170e5ba7cdbda47124a4a0abb953f83
jchaverot <jeremy.chaverot@epfl.ch>
2022-03-16 10:17:13 +0100

    fixed ckvs_utils.c

--- done/ckvs_utils.c
+++ done/ckvs_utils.c
@@ -2,20 +2,22 @@
 // Created by trous on 15.03.2022.
 //
 
+#include <stdio.h>
+#include "ckvs_utils.h"
+
 void print_SHA(const char *prefix, const struct ckvs_sha *sha) {
-    char buffer[SHA256_PRINTED_STRLEN];
-    SHA256_to_string(sha, buffer);
+    char buffer[SHA256_PRINTED_STRLEN] = {0};
+    SHA256_to_string(&sha, buffer);
     pps_printf("%-5s: %s\n", prefix, buffer);
 }
 
-
 void SHA256_to_string(const struct ckvs_sha *sha, char *buf) {
-    hex_encode(sha.sha, SHA256_DIGEST_LENGTH, buf);
+    hex_encode(sha->sha, SHA256_DIGEST_LENGTH, buf);
 }
 
 void hex_encode(const uint8_t *in, size_t len, char *buf) {
     for (size_t i = 0; i < len; ++i) {
-        sprintf(buf, "%02x", in));
+        sprintf(buf, "%02x", in);
     }
 }
 
