de39d0ac85d301aea6851ce4cd1bd6efe7cddc17
Albert Troussard <troussardalbert@gmail.com>
2022-05-16 10:21:27 +0200

    week 10 debug err

--- done/ckvs_client.h
+++ done/ckvs_client.h
@@ -53,3 +53,44 @@ int ckvs_client_new(const char *url, int optargc, char **optargv);
 
 
 
+/**
+ * @brief Retrives the ckvs from a json object.
+ *
+ * @param ckvs (struct ckvs*) the ckvs in which we put the read content
+ * @param obj (const json_object*) the json object we retrieve the information from
+ * @return int, error code
+ */
+int retrieve_ckvs_from_json(struct CKVS* ckvs, const struct json_object* obj);
+
+
+/**
+ * @brief Retrives the ckvs header from a json object.
+ *
+ * @param ckvs (struct ckvs*) the ckvs in which we put the read content for the header
+ * @param obj (const json_object*) the json object we retrieve the information from
+ * @return int, error code
+ */
+int retrieve_ckvs_header_from_json(struct CKVS *ckvs, const struct json_object *obj);
+
+/**
+ * @brief To get the usable string associated to the key from a json object.
+ *
+ * @param obj (const json_object*) the json object we retrieve the information from
+ * @param key (const char*) the key of the string of interest
+ * @param buf (char*) buffer used to store the computed string
+ * @return int, error code
+ */
+int get_string(const struct json_object* obj, const char* key, char* buf);
+
+/**
+ * @brief To get the integer associated to the key from a json object.
+ *
+ * @param obj (const json_object*) the json object we retrieve the information from
+ * @param key (const char*) the key of the integer of interest
+ * @param buf (char*) buffer used to store the computed integer
+ * @return int, error code
+ */
+int get_int(const struct json_object* obj, const char* key, int* buf);
+
+
+
