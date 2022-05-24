6149f1ee94766be62ff061fbc551909508da38f4
Jean-Cédric Chappelier <jean-cedric.chappelier@epfl.ch>
2022-05-24 16:48:21 +0200

    correction projet02

---
+++ grading/projet02/done/ckvs_io.c
@@ -0,0 +1,449 @@
+/**
+ * @file ckvs_io.c
+ * @brief c.f. ckvs_io.h
+ */
+#include <stdio.h>
+#include <string.h>
+#include "ckvs_utils.h"
+#include "ckvs.h"
+#include "ckvs_crypto.h"
+#include "error.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "ckvs_io.h"
+#include <stdlib.h>
+#include <openssl/sha.h>
+
+// ----------------------------------------------------------------------
+int ckvs_open(const char *filename, struct CKVS *ckvs) {
+    //check pointers
+    // correcteur : M_REQUIRE_NON_NULL
+    if (ckvs == NULL || filename == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    //initialize the CKVS database
+    memset(ckvs, 0, sizeof(struct CKVS));
+
+    //open the file in read and write binary mode and assign it to the CKVS File
+    FILE *file = NULL;
+    file = fopen(filename, "r+b");
+    if (file == NULL) {
+        //error
+        return ERR_IO;
+    }
+    ckvs->file = file;
+
+    int a = read_header(ckvs);
+    if (a != ERR_NONE) {
+        // correcteur : ça marche avec le memset, mais comme les entries ne sont pas initialisées un fclose serait plus clair
+        ckvs_close(ckvs);
+        return a;
+    }
+
+    ckvs->entries = calloc(ckvs->header.table_size, sizeof(ckvs_entry_t));
+    if (ckvs->entries == NULL) {
+        ckvs_close(ckvs);
+        return ERR_OUT_OF_MEMORY;
+    }
+
+    size_t nb_ok3 = fread(ckvs->entries, sizeof(ckvs_entry_t), ckvs->header.table_size, file);
+    if (nb_ok3 != ckvs->header.table_size) {
+        //error
+        ckvs_close(ckvs);
+        return ERR_IO;
+    }
+
+    return ERR_NONE;
+}
+
+// ----------------------------------------------------------------------
+void ckvs_close(struct CKVS *ckvs) {
+    //check if the argument is valid, if so exit the function without doing anything
+    if (ckvs == NULL) {
+        //error
+        return;
+    }
+    //close to file of the CKVS and make it point to NULL
+    if (ckvs->file != NULL) {
+        fclose(ckvs->file);
+        ckvs->file = NULL;
+    }
+    //free entries and make it point to NULL
+    if (ckvs->entries != NULL) {
+        free(ckvs->entries);
+        ckvs->entries = NULL;
+    }
+}
+
+// ----------------------------------------------------------------------
+int ckvs_find_entry(struct CKVS *ckvs, const char *key, const struct ckvs_sha *auth_key, struct ckvs_entry **e_out) {
+    //check pointeurs
+    if (ckvs == NULL || key == NULL || auth_key == NULL || e_out == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    //booleans created to follow if the key was found in the CKVS database, and if found, check if the auth_key is correct
+    bool keyWasFound = false;
+    bool authKeyIsCorrect = false;
+
+    bool free_place_found = false;
+    uint32_t free_index = 0;
+
+    uint32_t idx = ckvs_hashkey(ckvs, key);
+
+    //iterate over the table from index hashkey in linear probing
+    uint32_t max_it = idx + ckvs->header.table_size;
+    for (uint32_t i = idx; i < max_it; ++i) {
+        // compute the index
+        // correcteur : & (size - 1), c'est une puissance de deux
+        uint32_t j = i % ckvs->header.table_size;
+        //check the key
+        if (strncmp(ckvs->entries[j].key, key, CKVS_MAXKEYLEN) == 0) {
+            keyWasFound = true;
+            //check the auth_key
+            if (ckvs_cmp_sha(&ckvs->entries[j].auth_key, auth_key) == 0) {
+                authKeyIsCorrect = true;
+                *e_out = &ckvs->entries[j];
+            }
+            break;
+            // for new check for an empty slot
+        } else if (!free_place_found && ckvs->entries[j].key[0] == '\0') {
+            free_place_found = true;
+            free_index = j;
+        }
+
+    }
+
+    if (!keyWasFound) {
+        //the entry that can be given for new
+        *e_out = &ckvs->entries[free_index];
+        //error
+        return ERR_KEY_NOT_FOUND;
+    }
+    if (!authKeyIsCorrect) {
+        //error
+        return ERR_DUPLICATE_ID;
+    }
+
+    return ERR_NONE;
+}
+
+// ----------------------------------------------------------------------
+int
+read_value_file_content(const char *filename, char **buffer_ptr, size_t *buffer_size) { //TODO : fclose(file) when error
+    //check pointers
+    if (filename == NULL || buffer_ptr == NULL || buffer_size == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    //create and open file
+    FILE *file = NULL;
+    file = fopen(filename, "rb"); //open in read binary mode
+    if (file == NULL) {
+        //error
+        return ERR_IO;
+    }
+    //place the pointer at the end of the file and check errors
+    int err = fseek(file, 0, SEEK_END);
+    if (err != ERR_NONE) {
+        close_RVFC(&file, buffer_ptr);
+        return ERR_IO;
+    }
+
+    //affect the string's length
+    long int size_temp = ftell(file);
+    if (size_temp == -1) {
+        //error
+        close_RVFC(&file, buffer_ptr);
+        return ERR_IO;
+    }
+    size_t size = (size_t) size_temp;
+
+    //place the pointer at the beginning of the file back
+    err = fseek(file, 0, SEEK_SET);
+    if (err != ERR_NONE) {
+        close_RVFC(&file, buffer_ptr);
+        return ERR_IO;
+    }
+
+    //create a buffer and check errors
+    *buffer_ptr = calloc(size + 1, sizeof(char)); //so the '\0' char fits
+    if (*buffer_ptr == NULL) {
+        //error
+        close_RVFC(&file, buffer_ptr);
+        return ERR_OUT_OF_MEMORY;
+    }
+
+    //read file and check errors
+    size_t nb = fread(*buffer_ptr, sizeof(char), size, file);
+    if (nb != size) {
+        //error
+        close_RVFC(&file, buffer_ptr);
+        return ERR_IO;
+    }
+    *buffer_size = size + 1; //update the buffer size to have the place for the final '\0'
+    (*buffer_ptr)[size] = '\0'; //to add the final '\0'
+
+    //close the opened file and finish
+    fclose(file);
+    file = NULL;
+    return ERR_NONE;
+}
+
+//---------------------------
+void close_RVFC(FILE **file, char **buffer_ptr) {
+    //check argument
+    if (file != NULL) {
+        //check if the file hasn't been close previously
+        if (*file != NULL) {
+            //close it
+            fclose(*file);
+            *file = NULL;
+        }
+    }
+    //check argument
+    if (buffer_ptr != NULL) {
+        //check if the buffer hasn't been free previously
+        if (*buffer_ptr != NULL) {
+            // correcteur : *buffer_ptr pourrait ne pas être NULL mais quand même invalide,
+            // correcteur : comme il est initialisé (ou non) par le caller de RVFC
+            //free it
+            free(*buffer_ptr);
+            *buffer_ptr = NULL;
+        }
+    }
+
+}
+
+// ----------------------------------------------------------------------
+int ckvs_write_entry_to_disk(struct CKVS *ckvs, uint32_t idx) { //TODO : add a fflush(entry) after the fwrite ?
+    //check ckvs pointer and validity of idx value
+    // correcteur : M_REQUIRE_NON_NULL
+    if (ckvs == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+    //place the pointer on the file on the right place and check errors
+    int err = fseek(ckvs->file, (long int) (idx * sizeof(struct ckvs_entry) + sizeof(ckvs_header_t)), SEEK_SET);
+    if (err != ERR_NONE) {
+        //error
+        return ERR_IO;
+    }
+
+    //write the entry and check errors
+    size_t nb2 = fwrite(&ckvs->entries[idx], sizeof(ckvs_entry_t), 1, ckvs->file);
+    if (nb2 != 1) {
+        //error
+        return ERR_IO;
+    }
+    return ERR_NONE;
+}
+
+// ----------------------------------------------------------------------
+int ckvs_write_updated_header_to_disk(struct CKVS *ckvs) {
+    //check ckvs pointer
+    if (ckvs == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    //place the pointer on the file on the right place and check errors
+    int err = fseek(ckvs->file, (long int) (sizeof(ckvs->header.header_string) + 3 * sizeof(uint32_t)), SEEK_SET);
+    if (err != ERR_NONE) {
+        //error
+        return ERR_IO;
+    }
+
+    //write the new number of entries in the header and check errors
+    size_t nb2 = fwrite(&ckvs->header.num_entries, sizeof(uint32_t), 1, ckvs->file);
+    if (nb2 != 1) {
+        //error
+        return ERR_IO;
+    }
+    return ERR_NONE;
+}
+
+// ----------------------------------------------------------------------
+int ckvs_write_encrypted_value(struct CKVS *ckvs, struct ckvs_entry *e, const unsigned char *buf, uint64_t buflen) {
+    //check pointers
+    if (ckvs == NULL || e == NULL || buf == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+    //to place the pointer at the end of the ckvs file
+    int err = fseek(ckvs->file, 0, SEEK_END);
+    if (err != ERR_NONE) {
+        //error
+        return ERR_IO;
+    }
+
+    //to assign the new values of c2, value_off and value_len
+    e->value_len = buflen;
+    // correcteur : pas de check d'erreur pour ftell
+    e->value_off = (size_t) ftell(ckvs->file);
+
+    //write at the end of the ckvs file the encrypted value
+    size_t nb_ok = fwrite(buf, sizeof(char), buflen, ckvs->file);
+    if (nb_ok != buflen) {
+        //error
+        return ERR_IO;
+    }
+    //flush to be sure
+    err = fflush(ckvs->file);
+    if (err != ERR_NONE) { // correcteur : pas ERR_NONE!
+        //error
+        return ERR_IO;
+    }
+
+    return compute_idx_and_write(e, ckvs);
+}
+
+// ----------------------------------------------------------------------
+int ckvs_new_entry(struct CKVS *ckvs, const char *key, struct ckvs_sha *auth_key, struct ckvs_entry **e_out) {
+    //check pointers
+    if (ckvs == NULL || key == NULL || auth_key == NULL || e_out == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    //verify that the entry can be added i.e. that there is still space in the table
+    if (ckvs->header.threshold_entries <= ckvs->header.num_entries) {
+        //error
+        return ERR_MAX_FILES;
+    }
+
+
+    //to find the right entry in the database to place the entry
+    int err = ckvs_find_entry(ckvs, key, auth_key, e_out);
+    if (err != ERR_KEY_NOT_FOUND) {
+        //error if an entry with the same key is found
+        return err == ERR_NONE ? ERR_DUPLICATE_ID : err;
+    }
+    //associate the key to this entry
+    strncpy((char *) &((*e_out)->key), key, CKVS_MAXKEYLEN);
+
+    //associate the auth_key
+    (*e_out)->auth_key = *auth_key;
+
+    err = compute_idx_and_write(*e_out, ckvs);
+    if (err != ERR_NONE) {
+        return err;
+    }
+
+    //add one to the number of entries
+    ckvs->header.num_entries += 1;
+
+    //write the change in the header for the number of entries in the file
+    err=ckvs_write_updated_header_to_disk(ckvs);
+    if (err != ERR_NONE) {
+        return err;
+    }
+
+    return ERR_NONE;
+}
+
+int compute_idx_and_write(struct ckvs_entry *e, struct CKVS *ckvs) {
+    if (e == NULL || ckvs == NULL) {
+        return ERR_INVALID_ARGUMENT;
+    }
+    //to modify the right entry in the ckvs table, index is obtained by subtracting the pointers
+    uint32_t idx = (uint32_t)(e - &ckvs->entries[0]);
+
+    return ckvs_write_entry_to_disk(ckvs, idx);
+
+}
+
+// ----------------------------------------------------------------------
+static uint32_t ckvs_hashkey(struct CKVS *ckvs, const char *key) {
+    //check pointers
+    if (ckvs == NULL || key == NULL) {
+        //error
+        return ERR_INVALID_ARGUMENT;
+    }
+
+    ckvs_sha_t key_sha;
+    uint32_t hashkey;
+
+    //compute SHA256 of key
+    SHA256((const unsigned char *) key, strlen(key), key_sha.sha);
+
+    //copy the 4 first bytes
+    memcpy(&hashkey, key_sha.sha, sizeof(uint32_t));
+
+    //initialize the mask
+    uint32_t mask = (uint32_t) ckvs->header.table_size - 1;
+
+    //apply the mask and return the value
+    return hashkey & mask;
+}
+
+//----------------------------------------------------------------------
+int read_header(CKVS_t *ckvs) {
+    //check pointers
+    if (ckvs == NULL || ckvs->file == NULL) {
+        return ERR_INVALID_ARGUMENT;
+    }
+    // correcteur : plus simple de fread le header directement
+
+    //read the header and that it was well-read
+    char header_str[CKVS_HEADERSTRINGLEN];
+    size_t nb_ok = fread(header_str, sizeof(char), CKVS_HEADERSTRINGLEN, ckvs->file);
+    if (nb_ok != CKVS_HEADERSTRINGLEN) {
+        //error
+        return ERR_IO;
+    }
+    //read the infos and that they were well-read
+    uint32_t infos[CKVS_UINT32_T_ELEMENTS] = {0};
+    size_t nb_ok2 = fread(infos, sizeof(uint32_t), 4, ckvs->file);
+    if (nb_ok2 != CKVS_UINT32_T_ELEMENTS) {
+        //error
+        return ERR_IO;
+    }
+    //check that the header start with the good prefix
+    if (strncmp(CKVS_HEADERSTRING_PREFIX, header_str, strlen(CKVS_HEADERSTRING_PREFIX)) != 0) {
+        //error
+        ckvs_close(ckvs);
+        return ERR_CORRUPT_STORE;
+    }
+
+    if (infos[0] != 1) {
+        //error
+        return ERR_CORRUPT_STORE;
+    }
+
+    //check that the table has a size power of 2
+    int a = check_pow_2(infos[1]);
+    if (a != ERR_NONE) {
+        return a;
+    }
+
+    //construct the header now that every field is safe
+    ckvs_header_t header = {
+            .version           =infos[0],
+            .table_size        =infos[1],
+            .threshold_entries =infos[2],
+            .num_entries       =infos[3]
+    };
+    strcpy(header.header_string, header_str);
+    ckvs->header = header;
+
+    return ERR_NONE;
+}
+
+//----------------------------------------------------------------------
+int check_pow_2(uint32_t table_size) {
+    // correcteur : ça marche, mais il y a plus simple
+    while (table_size >= 2) {
+        if (table_size % 2 != 0) break;
+        table_size = table_size / 2;
+    }
+    if (table_size != 1) {
+        //error
+        return ERR_CORRUPT_STORE;
+    }
+    return ERR_NONE;
+}
---
+++ grading/projet02/done/ckvs_io.h
@@ -0,0 +1,136 @@
+/**
+ * @file ckvs_io.h
+ * @brief ckvs_io - IO operations for a local database
+ * @author E Bugnion, A. Clergeot
+ */
+#pragma once
+
+#include <stdint.h> // for uint64_t
+#include "ckvs.h"
+
+/* *************************************************** *
+ * TODO WEEK 04: Define struct CKVS here               *
+ * *************************************************** */
+/**
+ * @brief Represents a CKVS database.
+ */
+struct CKVS {
+    struct ckvs_header header;
+    ckvs_entry_t *entries;
+    FILE *file;
+};
+typedef struct CKVS CKVS_t;
+
+/* *************************************************** *
+ * TODO WEEK 05: Open/close refactoring                *
+ * *************************************************** */
+/**
+ * @brief Opens the CKVS database at filename.
+ * Also checks that the database is valid, as described in 04.stats.md
+ *
+ * @param filename (const char*) the path to the database to open
+ * @param ckvs (struct CKVS*) the struct that will be initialized
+ * @return int, error code
+ */
+int ckvs_open(const char *filename, struct CKVS *ckvs);
+
+
+/**
+ * @brief helper function to read the header of ckvs
+ *
+ * @param ckvs (struct CKVS*) the struct that will be initialized
+ * @return int, error code
+ */
+int read_header(CKVS_t *ckvs);
+
+/**
+ * @brief helper function to check if a uint32_t is a power of 2
+ *
+ * @param (uint32_t) table_size the uint32_t to check
+ * @return int, error code
+ */
+int check_pow_2(uint32_t table_size);
+
+/**
+ * @brief Closes the CKVS database and releases its resources.
+ *
+ * @param ckvs (struct CKVS*) the ckvs database to close
+ */
+void ckvs_close(struct CKVS *ckvs);
+
+/* *************************************************** *
+ * TODO WEEK 05                                        *
+ * *************************************************** */
+/**
+ * @brief Finds the entry with the given (key, auth_key) pair in the ckvs database.
+ *
+ * @param ckvs (struct CKVS*) the ckvs database to search
+ * @param key (const char*) the key of the entry
+ * @param auth_key (const struct ckvs_sha*) the auth_key of the entry
+ * @param e_out (struct ckvs_entry**) points to a pointer to an entry. Used to store the pointer to the entry if found.
+ * @return int, error code
+ */
+int ckvs_find_entry(struct CKVS *ckvs, const char *key, const struct ckvs_sha *auth_key, struct ckvs_entry **e_out);
+
+/* *************************************************** *
+ * TODO WEEK 06                                        *
+ * *************************************************** */
+/**
+ * @brief Writes the already encrypted value at the end of the CKVS database,
+ * then updates and overwrites the entry accordingly.
+ *
+ * @param ckvs (struct CKVS*) the ckvs database to search
+ * @param e (struct ckvs_entry *e) the entry to which the secret belongs
+ * @param buf (const unsigned char*) the encrypted value to write
+ * @param buflen (uint64_t) the length of buf
+ * @return int, error code
+ */
+int ckvs_write_encrypted_value(struct CKVS *ckvs, struct ckvs_entry *e, const unsigned char *buf, uint64_t buflen);
+
+/**
+ * @brief Reads the file at filename, then allocates a buffer to dumps the file content into.
+ * Not asked to students but helpful to have
+ */
+int read_value_file_content(const char *filename, char **buffer_ptr, size_t *buffer_size);
+
+
+/**
+ * @brief helper function to close the file and free the buffer during the execution of read_value_file_content
+ *
+ * @param file (FILE**) file to close
+ * @param buffer_ptr (char **) buffer to free
+ */
+void close_RVFC(FILE **file, char **buffer_ptr);
+
+/* *************************************************** *
+ * TODO WEEK 07                                        *
+ * *************************************************** */
+/**
+ * @brief Creates a new entry in ckvs with the given (key, auth_key) pair, if possible.
+ *
+ * @param ckvs (struct CKVS*) the ckvs database to search
+ * @param key (const char*) the key of the new entry
+ * @param auth_key (const struct ckvs_sha*) the auth_key of the new entry
+ * @param e_out (struct ckvs_entry**) points to a pointer to an entry. Used to store the pointer to the created entry, if any.
+ * @return int, error code
+ */
+int ckvs_new_entry(struct CKVS *ckvs, const char *key, struct ckvs_sha *auth_key, struct ckvs_entry **e_out);
+
+/**
+ * @brief Computes the hashkey of a the given key in ckvs.
+ *
+ * @param ckvs (struct CKVS*) the ckvs database to search
+ * @param key (const char*) the key we want to compute the hash
+ * @return uint32_t, the hashkey
+ */
+static uint32_t ckvs_hashkey(struct CKVS *ckvs, const char *key);
+
+
+/**
+ *@brief compute the index of the given entry in ckvs->entries and write to the disk
+ * @param e (struct ckvs_entry*) entry to be writen
+ * @param ckvs (struct CKVS*) the ckvs database to search
+ * @return
+ */
+int compute_idx_and_write(struct ckvs_entry *e, struct CKVS *ckvs);
+
